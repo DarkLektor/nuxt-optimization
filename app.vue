518f300e8a08af2caa9cd1486ae1df2384c46b63
Kamil Sishev <darklektor1@gmail.com>
2024-06-14 14:03:40 +0500

    Init

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <div v-for="item in items" :key="item.id" class="item">
+      <h5>{{ item.title }}</h5>
+      <img height="400" width="400" :src="item.url" :alt="item.title">
+    </div>
+  </div>
+</template>
+<script setup>
+
+const items = ref([])
+fetch('https://jsonplaceholder.typicode.com/photos?_limit=100')
+    .then(response => response.json())
+    .then(json => items.value = json)
+</script>
+<style>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+}
+.item {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  object-fit: contain;
+}
+
+</style>
