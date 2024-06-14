6e2d3fadec4a17bcb12c6a6595570a4f1be5ef8b
Kamil Sishev <darklektor1@gmail.com>
2024-06-14 15:02:26 +0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,29 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
     port: 5000
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  srcDir: '.',
+  dir: {
+    app: 'app'
+  },
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
   }
 })
